fe0c7e88f09df029bf978d519a57419eb0b2bc4f
daanniill <daniil.malakhov12@gmail.com>
2025-03-23 17:14:26 -0700

    simple commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 client = OpenAI()
 
 response = client.chat.completions.create(
-    model='gpt-4-vision-preview',
+    model='gpt-4o-mini',
     messages = [
         {
             'role': 'user',
@@ -15,7 +15,8 @@ response = client.chat.completions.create(
                 {
                     'type' : 'image_url',
                     'image_url': {
-                        'url': 'https://static1.squarespace.com/static/607f89e638219e13eee71b1e/60a5de2d343ab05906685029/646c549369f8011c28cd5843/1684821591871/michael-sum-LEpfefQf4rU-unsplash.jpg?format=1500w'
+                        'url': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKfTzRiDc-HYxu6l5_jZpSqkOsctxH-oMesA&s',
+                        'detail': 'low',
                     },
                 },
             ],
